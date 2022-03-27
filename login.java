a82383273023329ce6b383ba8e0687dd3c418f82
zetwenty <fadhillahzainum@gmail.com>
2022-03-27 09:55:05 +0700

    login.java

---
+++ login.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class login {
+    public static void main(String[] args) {
+        String email, password;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Email : ");
+        email = input.nextLine();
+        System.out.print("Password : ");
+        password = input.nextLine();
+        userService satu = new userService(email, password);
+        satu.login();
+    }
+}
